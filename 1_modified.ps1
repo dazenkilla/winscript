5fcf1d053f6edafab9662781216addc2064b8e12
dazenkilla <eqpradipta@gmail.com>
2026-02-20 14:29:35 +0700

    Update PowerShell script for time zone configuration
    Added manual time zone setting and disabled auto-update.

--- 1_modified.ps1
+++ 1_modified.ps1
@@ -1,6 +1,7 @@
 # Set execution policy
 Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy Bypass -Force
 Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned -Force
+
 # Install necessary modules
 $modules = @("AuditPolicyDsc", "SecurityPolicyDsc", "PSDesiredStateConfiguration", "DellBIOSProvider")
 foreach ($module in $modules) {
@@ -13,5 +14,9 @@ if ($profile) {
     Set-NetConnectionProfile -InterfaceIndex $profile.InterfaceIndex -NetworkCategory Private
 }
 
+# 1. Set Time Zone secara manual ke Jakarta (GMT +7)
+Set-TimeZone -Id "SE Asia Standard Time"
+
+# 2. Disable Time Zone Auto Update (Mencegah perubahan otomatis)
 $TZAutoSettingRegPath = "HKLM:\SYSTEM\CurrentControlSet\Services\tzautoupdate"
 Set-ItemProperty -Path $TZAutoSettingRegPath -Name "Start" -Value 3
